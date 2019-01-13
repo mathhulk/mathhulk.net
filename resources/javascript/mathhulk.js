52f8c890158e173202ca284b100e39f6808e3c2f
mathhulk <no-reply@mathhulk.com>
2019-01-12 16:50:29 -0800

    New section for GitLab, Lukkit addition

--- resources/javascript/mathhulk.js
+++ resources/javascript/mathhulk.js
@@ -1,13 +1,13 @@
 $(document).ready(function( ) {
-	$.ajax({
-		headers: {
-			"Accept": "application/vnd.github.mercy-preview+json"
-		},
-		dataType: "json",
-		url: "https://api.github.com/users/mathhulk/repos",
-		success: function(repositories) {
-			$.get("resources/templates/repository.tpl", function(template) {
-				$(".parent .row").empty( );
+	$.get("resources/templates/repository.tpl", function(template) {
+		$.ajax({
+			headers: {
+				"Accept": "application/vnd.github.mercy-preview+json"
+			},
+			dataType: "json",
+			url: "https://api.github.com/users/mathhulk/repos",
+			success: function(repositories) {
+				$(".github .row").empty( );
 				
 				$.each(repositories, function(index, value) {
 					let repository = template;
@@ -25,9 +25,37 @@ $(document).ready(function( ) {
 					
 					repository = repository.replace("{{ html_url }}", value.html_url);
 					
-					$(".parent .row").append(repository);
+					$(".github .row").append(repository);
 				});
-			});
-		}
+			}
+		});
+		
+
+		$.ajax({
+			dataType: "json",
+			url: "https://gitlab.com/api/v4/users/mathhulk/projects",
+			success: function(repositories) {
+				$(".gitlab .row").empty( );
+				
+				$.each(repositories, function(index, value) {
+					let repository = template;
+					
+					repository = repository.replace("{{ name }}", value.name.replace("-", " &bull; "));
+					repository = repository.replace("{{ description }}", value.description);
+					
+					let topics = value.tag_list;
+					
+					$.each(topics, function(index, value) {
+						topics[index] = "<span class=\"badge badge-pill badge-primary\">" + value + "</span>";
+					});
+					
+					repository = repository.replace("{{ topics }}", topics.join(" "));
+					
+					repository = repository.replace("{{ html_url }}", value.web_url);
+					
+					$(".gitlab .row").append(repository);
+				});
+			}
+		});
 	});
 });
